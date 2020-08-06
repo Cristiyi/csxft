6296fd672ec66134eca4bc16bd50f24449e3776a
cristi <yipengfei0824@163.com>
2020-08-06 20:24:11 +0800

    1

--- model/notice.go
+++ model/notice.go
@@ -11,6 +11,7 @@ import "github.com/jinzhu/gorm"
 type Notice struct {
 	gorm.Model
 	ProjectId uint `gorm:"not null;index:idx_project_id"`
+	BatchId uint `gorm:"not null;index:idx_batch_id"`
 	Type int32
 	NoticeType int32
 	Content string
