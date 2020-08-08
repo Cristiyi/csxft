48350f2a3b1dc4737f5f1e3ce55bc77cab5bfca2
cristi <yipengfei0824@163.com>
2020-08-08 20:26:13 +0800

    test

--- model/project.go
+++ model/project.go
@@ -60,7 +60,7 @@ type Project struct {
 	IsNearLineFouth  int32  `gorm:"default:0"`
 	IsNearLineFifth  int32  `gorm:"default:0"`
 	IsNearLineSixth  int32  `gorm:"default:0"`
-	PredictCredDate int
+	PredictCredDate time.Time
 	Tag string
 	//效果图
 	EffectImages []Image `gorm:"ForeignKey:ProjectId"`
